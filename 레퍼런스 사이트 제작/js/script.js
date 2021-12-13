426f317712522949ddbbdcf06bb7512ecb0ac637
kimbora131 <kimbora131@gmail.com>
2021-12-13 17:57:42 +0900

    Day5

--- 레퍼런스 사이트 제작/js/script.js
+++ 레퍼런스 사이트 제작/js/script.js
@@ -15,4 +15,39 @@ $(function(){
         pauseOnHover:false,         //마우스 오버 시 멈춤 여부
         pauseOnFocus:false          //포커스시 멈춤 여부
     })
+
+    //모바일 네비게이션
+    $('header .open').on('click', function() {
+        $('body').css({'overflow' :  'hidden'})
+        $('header .bg').css({'display' :  'block'})
+        $('header nav').addClass('on')
+    })
+
+    //X버튼 클릭 이벤트
+    $('header .close').on('click', function() {
+        $('body').css({'overflow' :  'auto'})
+        $('header .bg').css({'display' :  'none'})
+        $('header nav').removeClass('on')
+    })
+
+
+    //헤더 스크롤 배경 넣기
+    let scrollValue = 0
+    scrollValue = $(document).scrollTop()
+
+
+    function fixHeader() {
+        //스크롤 위치가 200px 초과하면, 배경 넣어주기
+        if(scrollValue > 200) {
+            $('header').addClass('on')
+        }else{
+            $('header').removeClass('on')
+        }
+    }
+
+    //스크롤 위치 감지 이벤트
+    $(window).ㅆon('scroll resize', function(){
+        scrollValue = $(document).scrollTop()
+        fixHeader()
+    })
 })
