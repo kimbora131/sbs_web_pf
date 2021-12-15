b8a775f5ab2d6129b6c254d6e9417485da681ff8
kimbora131 <kimbora131@gmail.com>
2021-12-15 16:02:39 +0900

    Day6

--- 레퍼런스 사이트 제작/js/script.js
+++ 레퍼런스 사이트 제작/js/script.js
@@ -19,15 +19,17 @@ $(function(){
     //모바일 네비게이션
     $('header .open').on('click', function() {
         $('body').css({'overflow' :  'hidden'})
-        $('header .bg').css({'display' :  'block'})
+        $('header .bg').fadeIn(1500)
         $('header nav').addClass('on')
+        $('header nav').css({'width':'300px'})
     })
 
     //X버튼 클릭 이벤트
-    $('header .close').on('click', function() {
+    $('header .close, header .bg').on('click', function() {
         $('body').css({'overflow' :  'auto'})
-        $('header .bg').css({'display' :  'none'})
+        $('header .bg').fadeOut(1500);
         $('header nav').removeClass('on')
+        $('header nav').css({'width':'0'})
     })
 
 
@@ -46,7 +48,9 @@ $(function(){
     }
 
     //스크롤 위치 감지 이벤트
-    $(window).ㅆon('scroll resize', function(){
+    $(window).on('scroll resize', function(){
+        const scrollTop = $(document).scrollTop()
+        console.log(scrollTop);
         scrollValue = $(document).scrollTop()
         fixHeader()
     })
